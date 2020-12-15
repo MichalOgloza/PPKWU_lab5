bfebf58746c5a32b96a4dbe7fda3755a41782b47
Michal <michalogloza97@gmail.com>
2020-12-15 15:17:30 +0100

    extracted company names using BeautifulSoup

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +12,12 @@ def home():
 
 @app.route('/find_company/<key_word>')
 def find(key_word):
+    html = requests.get("https://panoramafirm.pl/szukaj?k={keyword}&l=".format(keyword=key_word)).text
+    soup = BeautifulSoup(html, features="html.parser")
+    lst = soup.find('ul', {"id": "company-list"})
+    for item in lst.find_all('li', {"class": "company-item"}, recursive=False):
+        name = item.find('div', {"class": "company-top-content"}).find('div').find('h2').find('a').get_text()
+        print(name)
     return key_word
 
 
